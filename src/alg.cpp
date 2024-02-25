8fb5fa3685490a8f99e9a409bc4515c1c1e7e715
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-02-25 20:14:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,51 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value <= 1) {
+        return false; 
+    }
+    if (value == 2) {
+        return true; 
+    }
+    if (value % 2 == 0) {
+        return false; 
+    }
+    for (uint64_t i = 3; i <= value/2; i ++) {
+        if (value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    uint64_t count = 0;
+    uint64_t number = 2;
+    while (count < n) {
+        if (checkPrime(number)) {
+            count++;
+        }
+        number++;
+    }
+    return number - 1; 
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    int number = value + 1;
+    while (true) {
+        if (checkPrime(number)) {
+            return number;
+        }
+        number++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int sum = 0;
+    for (int i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
